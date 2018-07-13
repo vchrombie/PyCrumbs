e14e7ca3055f8136d400ca82b0653e3b77e6458b
vchrombie <venuvardhanreddytekula8@gmail.com>
2018-07-13 22:17:41 +0530

    Initial commit

---
+++ g4g-to-pdf/g4g.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import requests
+
+from os import system
+from sys import exit
+from time import sleep
+
+
+
+BASE_URL = 'http://www.geeksforgeeks.org/'
+articles = []
+
+
+if __name__ == '__main__':
